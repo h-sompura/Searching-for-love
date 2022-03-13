f347430384a3405b795a0063c786e4a016856aa0
Urja Desai <42894876+urja1112@users.noreply.github.com>
2022-03-13 19:17:14 +0530

    Add files via upload

--- SearchingforLove/main.swift
+++ SearchingforLove/main.swift
@@ -1,14 +1,17 @@
+//
+//  main.swift
+//  SearchingforLove
+//
+//  Created by Urja Desai on 01/03/22.
+//
+
 import Foundation
 
-func printLineSeperator() {
-    print("")
-    print("-------------------------------")
-    print("")
-}
 //creating Hero Hugie
+var turn = 1
 let gameHero = Hero(name: "Hugie")
-printLineSeperator()
-//intro for the game
+print("-------------------------------")
+print("")
 print("> \(gameHero.name), it is the day before your wedding. You are very much excited to be getting married to the love of your life, Astrid.")
 print("  But wait... a little birdy informs you that an evil wizard has kidnapped Astrid and fled to a castle somewhere far away and unknown.")
 print("")
@@ -16,7 +19,8 @@ print("> Do not worry though! We will help in your quest to save Astrid.")
 print("> We dropped a map next to you which will help with searching for Astrid.")
 print("> Remember! This world is full of monsters which you have to defeat to move forward.")
 print("> Good luck, \(gameHero.name). We hope you succeed in searching for your love! :) ")
-printLineSeperator()
+print("")
+print("-------------------------------")
 
 
 //creating Monsters
@@ -28,7 +32,7 @@ let monstersList = [
     Monster(name: "Fetidbrute")
 ]
 
-//creating a list of locations with a monster associated
+//creating a list of locations
 let locationsList = [
     Location(locationName: "Ithaca", monster: monstersList[0]),
     Location(locationName: "Sparta", monster: monstersList[1]),
@@ -44,90 +48,115 @@ let roadsList = [
     Road(startingLocation: "Sparta", endingLocation: "Mycanae", roadType: roadType.Swampy),
     Road(startingLocation: "Argos", endingLocation: "Athens", roadType: roadType.Mountainous),
     Road(startingLocation: "Mycanae", endingLocation: "Athens", roadType: roadType.Paved),
-    //Road(startingLocation: "Athens", endingLocation: "Athens",roadType: roadType.Paved), //could we change ending to string optional here or do we even need to connect ending location??***
+    Road(startingLocation: "Athens", endingLocation: "Athens",roadType: roadType.Paved), //could we change ending to string optional here??***
 ]
 
 //creating a map using locations list and roads list
 let map = Map(locations: locationsList, roads: roadsList, placeAstrid:"Athens")
-printLineSeperator()
 map.printMap()
 //this prints the easiest path ---> map.takeJourney(startingLocation: "Ithaca", endingLocation: "Athens")
 
+
 var isAstridFound:Bool = false
-var valid = true //By default true to repeat the condition
 var searchCaseString = "Search for Astrid"
-printLineSeperator()
+
+var valid = true //By default true to repeat the condition
 repeat
 {
-    print("> \(gameHero.name), what will you do?")
-    if(isAstridFound)
+    print("Hello..!! Welcome to Searching For Love Game")
+    print("What will you do")
+    if(isAstridFound == true)
     {
-        searchCaseString = "Search for Astrid [COMPLETE!]"
+    print("1. Search for Astrid" + "[Complete!]")
     }
-    print("\t 1. \(searchCaseString)")
-    print("\t 2. Rescue Astrid")
-    print("\t 3. Quit")
+    print("1. Search for Astrid")
+    print("2. Rescue Astrid")
+    print("3. Quit")
     let selection = Int(readLine()!)
     switch (selection) {
         case 1:
-            print("> Searching for Astrid....")
-            isAstridFound = map.searchAstrid(startingLocation: "Ithaca")
-            if(isAstridFound == false) {
-                print("")
-                print("> Astrid is not on the map!")
-                printLineSeperator()
-            }
-//            map.traverse(startingLocation: "Ithaca")
-
+        print("> Searching for Astrid....")
+                   isAstridFound = map.searchAstrid(startingLocation: "Ithaca")
+                   if(isAstridFound == false) {
+                       print("")
+                       print("> Astrid is not on the map!")
+                   }
         case 2:
-            var giveUp = true
-            print("> Rescuing Astrid....")
+            print("Rescuing Astrid....")
             if(isAstridFound)
             {
-                print("> already Know the location of astrid")
-//                let m1 = Monsters(name: "Xyz")
-                //print("main",m1.abilityToAttack)
-                //print("main",m1.maxHealthPoint)
-                //let l1 = Locations(locationName: "abc",monster: m1)
-                // l1
-                repeat
+                print(locationsList.capacity)
+                print("already Know the location of astrid")
+                
+                var m1 : Location?
+                             for i in locationsList
+                             {
+                                 print("\(i)")
+                                 print(i.monsters.maxHealthPoints)
+                                 m1 = i
+                             }
+                let f1 =  Fight.init(hero: gameHero, monster: m1!.monsters)
+                
+                print("Hero's Max",f1.playerHero.maxHealthPoints)
+                print("Monster's Max",f1.playerMonster.maxHealthPoints)
+                print("hero's wapon",f1.playerHero.weaponStrength)
+
+                while(f1.playerHero.maxHealthPoints >= 0 && f1.playerMonster.maxHealthPoints >= 0 && f1.playerMonster.maxHealthPoints >= f1.playerHero.weaponStrength)
                 {
-                    print("> Hugie, what move will you make?")
-                    if(true)
+                    if(f1.playerMonster.maxHealthPoints >= f1.playerHero.weaponStrength)
                     {
-                    print("\t> 1. Attack")
-                    print("\t> 2. Sneak")
-                    print("\t> 3. Give Up")
-                    
-                    let selection = Int(readLine()!)
-                    switch(selection)
-                        {
-                            case 1:
-                                print("Attack")
-                                //gameHero.takeDamage(amt: m1.abilityToAttack)
-                                print("Damage taken",gameHero.attack())
-                                print(gameHero.maxHealthPoints)
-                                //m1.takeDamage(amt: gameHero.weaponStrength)
-                                //print(m1.attack())
-                            case 2:
-                                print("Sneak")
-                            case 3:
-                                giveUp = false
-                                print("Monster wins..Hugie Gave up..!!")
-                                print("Game Over")
-                                valid = false
-                            default :
-                                print("Default")
-                        }
+                        print("Location change")
+                        f1.playerMonster.maxHealthPoints = 0
                     }
-                }while(giveUp == true)
+                print("in while")
+                if(turn == 1)
+                {
+                        f1.performTurn(abc: action_perform.attack, turn: 1)
+                        print(f1.playerMonster.maxHealthPoints)
+                        print(f1.playerHero.maxHealthPoints)
+            
+                }
+                else
+                {
+                    f1.performTurn(abc: action_perform.attack, turn: 2)
+                    print("max point in 2  ", f1.playerMonster.maxHealthPoints)
+                    print("max point in 2 for hero",f1.playerHero.maxHealthPoints)
+                    print("in else")
+                    turn = 1
+                }
+                
+                }
+                
+                
+//                var m1 : Location?
+//                for i in locationsList
+//                {
+//                    print("\(i)")
+//                    print(i.monsters.maxHealthPoints)
+//                    m1 = i
+//                }
+//                while(m1!.monsters.maxHealthPoints >= 0 && gameHero.maxHealthPoints >= 0)
+//                {
+//                    if turn == 1
+//                    {
+//                        print("hugie's turn")
+//                        m1?.monsters.maxHealthPoints = m1?.monsters.maxHealthPoints ?? 0  - gameHero.weaponStrength
+//                        turn = 2
+//                    }
+//                    else
+//                    {
+//                        print("monster's turn")
+//                        gameHero.maxHealthPoints = gameHero.maxHealthPoints - (m1?.monsters.attackPower)! ?? 0
+//                        turn = 1
+//                    }
+//                }
             }
             else
             {
             print("Please select option 1 from menu and Search For Astrid's Location")
             }
         case 3:
-            print("Bye!")
+            print("bye")
             valid = false // make it false to close the game
             break
         default:
@@ -136,12 +165,3 @@ repeat
 }while(valid == true)
 
 
-//Roads list for a different map in case we need to show easiest path logic - works yay!
-
-//Road(startingLocation: "Ithaca", endingLocation: "Argos", roadType: roadType.Paved),
-//Road(startingLocation: "Ithaca", endingLocation: "Mycanae", roadType: roadType.Swampy),
-//Road(startingLocation: "Argos", endingLocation: "Lokris", roadType: roadType.Mountainous),
-//Road(startingLocation: "Argos", endingLocation: "Salamis", roadType: roadType.Swampy),
-//Road(startingLocation: "Lokris", endingLocation: "Athens", roadType: roadType.Paved),
-//Road(startingLocation: "Salamis", endingLocation: "Athens", roadType: roadType.Paved),
-//Road(startingLocation: "Mycanae", endingLocation: "Athens", roadType: roadType.Paved),
